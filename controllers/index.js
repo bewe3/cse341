e34f8e15b97fa3abd836fc6de0194efd73aa4295
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-04-29 15:11:59 -0600

    let's try this

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
+const mongodb = require('../db/connect');
 
-const awesomeFunction = (req, res, next) => {
-    res.json('Awesome person');
+const getData = async (req, res, next) => {
+    const result = await mongodb.getDb().db('dataset').collection('contacts').find();
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
 };
 
-module.exports = {awesomeFunction};
+module.exports = { getData };
